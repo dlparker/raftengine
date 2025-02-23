8bc966a600b781da1801e7b0b7d06f40a6625990
Dennis Parker <dennis@windhavengroup.com>
2025-02-23 16:17:41 -0600

    updated timer tests to more determintically catch candidate experiencing election timeout

--- tests/test_log_fiddles.py
+++ tests/test_log_fiddles.py
@@ -22,7 +22,7 @@ extra_logging = [dict(name=__name__, level="debug"), dict(name="Triggers", level
 #extra_logging = [dict(name=__name__, level="debug"),]
 log_config = setup_logging(extra_logging)
 
-async def test_empty_log(cluster_maker):
+async def test_empty_log_1(cluster_maker):
     cluster = cluster_maker(3)
     # do real timer values, but start disabled
     heartbeat_period=0.005
@@ -51,8 +51,6 @@ async def test_empty_log(cluster_maker):
         sequence2 = SNormalCommand(cluster, "add 1", 1)
         command_result = await cluster.run_sequence(sequence2)
 
-        #await cluster.deliver_all_pending()
-    await cluster.start_auto_comms()
     assert ts_1.operations.total == 50
     # Now "crash" the leader, run an election, then have
     # the leader come up with an empty log
@@ -61,6 +59,7 @@ async def test_empty_log(cluster_maker):
     ts_1.operations.total = 0
     ts_1.block_network()
 
+    await cluster.start_auto_comms()
     await ts_2.enable_timers()
     await ts_3.enable_timers()
     logger.info('------------------------ Running Partial Election')
@@ -68,8 +67,7 @@ async def test_empty_log(cluster_maker):
     await cluster.run_sequence(sequence)
 
     # old leader is now ignorant of all past
-    ts_1.hull.log = MemoryLog()
-    ts_1.hull.state.log = MemoryLog()
+    ts_1.replace_log()
     await ts_1.hull.state.start()
 
     # let the timers run so it things catch up normally
@@ -83,4 +81,48 @@ async def test_empty_log(cluster_maker):
     assert ts_1.operations.total == 50        
     await cluster.stop_auto_comms()
     logger.debug('------------------------ Tardy follower caught up ---')
+
+async def test_empty_log_2(cluster_maker):
+    cluster = cluster_maker(3)
+    # do real timer values, but start disabled
+    heartbeat_period=0.005
+    election_timeout_min=0.09
+    election_timeout_max=0.11
+    config = cluster.build_cluster_config(heartbeat_period=heartbeat_period,
+                                          election_timeout_min=election_timeout_min,
+                                          election_timeout_max=election_timeout_max)
+    cluster.set_configs(config)
+
+    uri_1, uri_2, uri_3 = cluster.node_uris
+    ts_1, ts_2, ts_3 = [cluster.nodes[uri] for uri in [uri_1, uri_2, uri_3]]
+    logger = logging.getLogger(__name__)
+    await cluster.start()
+    await ts_1.hull.start_campaign()
+    sequence = SNormalElection(cluster, 1)
+    await cluster.run_sequence(sequence)
+    
+    assert ts_1.hull.get_state_code() == "LEADER"
+    assert ts_2.hull.state.leader_uri == uri_1
+    assert ts_3.hull.state.leader_uri == uri_1
+    logger = logging.getLogger(__name__)
+    logger.info('------------------------ Election done')
+
+    # Now "crash" the a follower and clear its log
+    ts_2.block_network()
+    await ts_2.hull.state.stop()
+    ts_2.replace_log()
+    await ts_2.hull.state.start()
+    assert await ts_2.log.get_last_index() == 0
+    ts_2.unblock_network()
+    await cluster.start_auto_comms()
+    await ts_1.enable_timers()
+
+    logger.info('------------------------ ts_2 "crash" and restart done')
+    start_time = time.time()
+    while (time.time() - start_time < heartbeat_period * 2 
+           and await ts_2.log.get_last_index() != 1):
+        await asyncio.sleep(0.0001)
+    assert await ts_2.log.get_last_index() == 1
+    await cluster.stop_auto_comms()
+    logger.debug('------------------------ Follower caught up ---')
 
--- tests/test_timers_1.py
+++ tests/test_timers_1.py
@@ -52,7 +52,6 @@ async def test_heartbeat_1(cluster_maker):
     assert full_out_ledger[1].get_code() == AppendResponseMessage.get_code()
     assert full_in_ledger[1].get_code() == AppendResponseMessage.get_code()
 
-        
 async def test_heartbeat_2(cluster_maker):
     cluster = cluster_maker(3)
     heartbeat_period = 0.02
@@ -110,7 +109,8 @@ async def test_lost_leader_1(cluster_maker):
     assert ts_3.hull.get_state_code() == "LEADER"
     assert ts_1.hull.state.leader_uri == uri_3
     assert ts_2.hull.state.leader_uri == uri_3
-
+    await ts_3.hull.demote_and_handle(None)
+    
     # Test that election happens in appoximately the leader_lost timeout
     start_time = time.time()
     fraction = election_timeout_max/10.0
@@ -121,7 +121,6 @@ async def test_lost_leader_1(cluster_maker):
             or ts_3.hull.state.state_code == "LEADER"):
             break
         await asyncio.sleep(fraction)
-    
     assert (ts_1.hull.state.state_code == "LEADER"
             or ts_2.hull.state.state_code == "LEADER"
             or ts_3.hull.state.state_code == "LEADER")
@@ -131,4 +130,62 @@ async def test_lost_leader_1(cluster_maker):
             leader = ts
             leader_uri = ts.uri
             break
+    assert await leader.log.get_term() > 1
     
+async def test_election_timeout_1(cluster_maker):
+    cluster = cluster_maker(3)
+    heartbeat_period = 0.001
+    election_timeout_min = 0.009
+    election_timeout_max = 0.011
+    config = cluster.build_cluster_config(heartbeat_period=heartbeat_period,
+                                          election_timeout_min=election_timeout_min, 
+                                          election_timeout_max=election_timeout_max)
+    cluster.set_configs(config)
+    uri_1, uri_2, uri_3 = cluster.node_uris
+    ts_1, ts_2, ts_3 = [cluster.nodes[uri] for uri in [uri_1, uri_2, uri_3]]
+
+    logger = logging.getLogger(__name__)
+    await cluster.start(timers_disabled=True)
+    await ts_1.hull.start_campaign()
+    sequence = SNormalElection(cluster, 1)
+    await cluster.run_sequence(sequence)
+
+    assert ts_1.hull.get_state_code() == "LEADER"
+    assert ts_2.hull.state.leader_uri == uri_1
+    assert ts_2.hull.state.leader_uri == uri_1
+
+    # Now arrange another election and make sure that the candidate
+    # experiences a timeout, no votes in required time. It should
+    # incr the term and retry so we can monitor by term value
+    orig_term = await ts_2.log.get_term()
+    ts_1.block_network()
+    ts_3.block_network()
+    await ts_1.hull.demote_and_handle(None)
+    await cluster.start_auto_comms()
+    await ts_2.enable_timers()
+    start_time = time.time()
+    fraction = election_timeout_max/50.0
+    while (time.time() - start_time < election_timeout_max  * 2
+           and await ts_2.log.get_term() == orig_term):
+        await asyncio.sleep(fraction)
+    term_now = await ts_2.log.get_term()
+    assert term_now != orig_term
+    if term_now == orig_term + 1:
+        # not done yet, just running as candidate for first time,
+        start_time = time.time()
+        while (time.time() - start_time < election_timeout_max  * 2
+               and await ts_2.log.get_term() == term_now):
+            await asyncio.sleep(fraction)
+    final_term = await ts_2.log.get_term()
+    assert final_term == orig_term + 2
+
+    ts_1.unblock_network()
+    ts_3.unblock_network()
+    # now make sure it can win
+    start_time = time.time()
+    while (time.time() - start_time < election_timeout_max  * 2
+           and ts_2.hull.state.state_code != "LEADER"):
+            await asyncio.sleep(fraction)
+    assert ts_2.hull.state.state_code == "LEADER"
+    
+    await cluster.stop_auto_comms()
