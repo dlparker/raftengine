1d2bfb7ec2a301ca0458e78b00df863a5e11fef7
Dennis Parker <dennis@windhavengroup.com>
2025-04-09 14:01:34 -0500

    more work on new tests

--- dev_tools/stepper.py
+++ dev_tools/stepper.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 from dev_tools.cluster_states import (NodeState, LogState, ActionOnState, ActionOnMessage,
                                       ValidateState, MessageCode, CommsOp, CommsEdge, ActionCode, Sequence, DoNow,
-                                      PhaseStep, NoOp, Phase)
+                                      PhaseStep, NoOp, Phase, ClusterState, RunState, RoleCode, NetworkMode, PhaseResult)
 from dev_tools.servers import PausingCluster, PausingServer, PauseTrigger
 from raftengine.messages.append_entries import AppendEntriesMessage, AppendResponseMessage
 from raftengine.messages.request_vote import RequestVoteMessage, RequestVoteResponseMessage
@@ -33,6 +33,9 @@ class WhenMsgOp(PauseTrigger):
         msg = f"{self.__class__.__name__} {self.message_code} {self.comms_op.comms_edge}"
         return msg
 
+    def connected_nodes(self, server):
+        return len(server.network.nodes)
+    
     async def is_tripped(self, server):
         done = False
         if self.comms_op.comms_edge in [CommsEdge.before_send,
@@ -63,19 +66,26 @@ class WhenMsgOp(PauseTrigger):
                 return done
             message = server.in_messages[0]
             if message.get_code() == self.message_code:
-                self.trigger_message = message
                 if self.comms_op.comms_edge == CommsEdge.after_all_responses:
                     # looking to get one from each other node, so count until met
-                    self.got_count += 1
-                    if self.got_count == self.total_nodes - 1:
+                    # but be aware that we can get called more than once
+                    # with the same message pending, as the run_till_trigger loop
+                    # does not guarantee that our input message will be handled
+                    # on this pass, it might do an output message
+                    if message != self.trigger_message:
+                        self.got_count += 1
+                    if self.got_count == self.connected_nodes(server) - 1:
                         # make sure it gets handled before pause
                         await server.network.do_next_in_msg(server)
                         done = True
+                    self.trigger_message = message
                 elif self.comms_op.comms_edge == CommsEdge.after_handle:
+                    self.trigger_message = message
                     # make sure it gets handled before pause
                     await server.network.do_next_in_msg(server)
                     done = True
                 else:
+                    self.trigger_message = message
                     done = True
         return done
     
@@ -134,18 +144,26 @@ class Sequencer:
         for node in self.sequence.start_state.nodes:
             # this will blow up if someone messes up the node it to uri logic
             server = self.cluster.nodes[node.uri]
+            server.start_saving_messages()
             self.node_records[node.uri] = NodeRecord(node, server)
+        self.current_state = self.sequence.start_state
         self.nets = None # will be a list of networks if they are split
-    
+
     async def run_sequence(self):
+        results = []
         phase = self.sequence.next_phase()
         while phase:
             await self.run_phase(phase)
+            phase_index = self.sequence.phases.index(phase)
+            cluster_state = await self.build_cluster_state()
+            self.sequence.save_state(cluster_state)
+            phase_result = PhaseResult(phase, phase_index, cluster_state)
+            results.append(phase_result)
             if self.phase_done_callback:
-                phase_index = self.sequence.phases.index(phase)
-                await self.phase_done_callback(phase, phase_index)
+                await self.phase_done_callback(phase_result)
             phase = self.sequence.next_phase()
-        
+        return results
+    
     async def run_phase(self, phase):
         found = []
         awaitables = []
@@ -216,17 +234,26 @@ class Sequencer:
         
     async def split_from_main_net(self, rec):
         if self.nets is None:
-            self.cluster.split_network()
+            main = {}
+            split = {}
+            split[rec.server.uri] = rec.server
+            for uri, orec in self.node_records.items():
+                if uri != rec.server.uri:
+                    main[orec.server.uri] = orec.server
+            self.cluster.split_network([main, split])
             self.nets = []
             self.nets.append(self.cluster.net_mgr.get_majority_network())
             self.nets.extend(self.cluster.net_mgr.get_minority_networks())
-            min_1 = self.nets[1]
-            min_1.add_node(rec.server)
         
     async def rejoin_main_net(self, rec):
         main_net = self.cluster.net_mgr.get_majority_network()
         if rec.server.network != main_net:
-            main_net.add_node(rec.server)
+            if len(rec.server.network.nodes) == 1:
+                self.cluster.unsplit()
+            else:
+                cur_net = rec.server.network
+                cur_net.remove_node(rec.server)
+                main_net.add_node(rec.server)
             
     async def crash(self, rec):
         await rec.server.simulate_crash()
@@ -258,6 +285,50 @@ class Sequencer:
         assert node_op.log_state.last_term == last_term
         assert node_op.log_state.commit_index == commit_index
 
+    async def build_cluster_state(self):
+        nodes = []
+        for uri,node in self.node_records.items():
+            nodes.append(await self.build_node_state(uri))
+        return ClusterState(nodes)
+    
+    async def build_node_state(self, node_uri):
+        rec = self.node_records[node_uri]
+        nid = rec.orig_state.node_id
+        server = rec.server
+        log = server.get_log()
+        term = await log.get_term()
+        index = await log.get_last_index()
+        last_term = await log.get_last_term()
+        commit_index = await log.get_commit_index()
+        log_state = LogState(term=term,
+                             index=index,
+                             last_term=last_term,
+                             commit_index=commit_index)
+        role = server.get_state_code()
+        if server.am_crashed:
+            run_state = RunState.crashed
+        else:
+            run_state = RunState.paused
+        if role.lower() == "leader":
+            role_code = RoleCode.leader
+        elif role.lower() == "candidate":
+            role_code = RoleCode.candidate
+        elif role.lower() == "follower":
+            role_code = RoleCode.follower
+        if server.network == server.cluster.net_mgr.get_majority_network():
+            net_mode = NetworkMode.majority
+        else:
+            net_mode = NetworkMode.minority
+        messages = server.get_saved_messages()
+        return NodeState(node_id=nid,
+                        role=role_code,
+                        run_state=run_state,
+                        network_mode=net_mode,
+                        log_state=log_state,
+                        uri=node_uri,
+                        messages=messages)
+        
+
 class StandardElectionSequence(Sequence):
 
     def __init__(self, node_count):
@@ -265,15 +336,15 @@ class StandardElectionSequence(Sequence):
 
     def do_setup(self):
 
-        node_1 = self.nodes_by_id[1]
+        node_1 = self.node_by_id(1)
         phase_1_steps = []
         do_now = DoNow(ActionCode.start_campaign,
                        description="Node 1 starts campaign as though election timeout has occured")
         ps = PhaseStep(node_1.uri, do_now_class=do_now)
         phase_1_steps.append(ps)
-        for nid in self.nodes_by_id:
+        for nid in self.uris_by_id:
             if nid != 1:
-                node = self.nodes_by_id[nid]
+                node = self.node_by_id(nid)
                 phase_1_steps.append(PhaseStep(node.uri, do_now_class=NoOp()))
         phase_1 = Phase(phase_1_steps, description="Starts campaign at node 1, others do nothing, ensuring node 1 wins election")
         self.add_phase(phase_1)
@@ -285,9 +356,9 @@ class StandardElectionSequence(Sequence):
         phase_2_steps.append(ps)
         comms_op_2b = CommsOp(MessageCode.append_entries_response, CommsEdge.after_send)
         action_2b = ActionOnMessage(comms_op=comms_op_2b, action_code=ActionCode.pause)
-        for nid in self.nodes_by_id:
+        for nid in self.uris_by_id:
             if nid != 1:
-                node = self.nodes_by_id[nid]
+                node = self.node_by_id(nid)
                 ps = PhaseStep(node.uri, runner_class=action_2b)
                 phase_2_steps.append(ps)
         phase_2 = Phase(phase_2_steps,
@@ -298,9 +369,9 @@ class StandardElectionSequence(Sequence):
         do_now = DoNow(ActionCode.send_heartbeats, description="Node 1 sends heartbeats to followers")
         ps = PhaseStep(node_1.uri, do_now_class=do_now)
         phase_3_steps.append(ps)
-        for nid in self.nodes_by_id:
+        for nid in self.uris_by_id:
             if nid != 1:
-                node = self.nodes_by_id[nid]
+                node = self.node_by_id(nid)
                 phase_3_steps.append(PhaseStep(node.uri, do_now_class=NoOp()))
         phase_3 = Phase(phase_3_steps, description="Leader queues heartbeats to followers so they can see commit")
         self.add_phase(phase_3)
@@ -311,10 +382,22 @@ class StandardElectionSequence(Sequence):
         desc = "Leader runs until it has handled one of the pending append entries response messages"
         ps = PhaseStep(node_1.uri, runner_class=action_4, description=desc)
         phase_4_steps.append(ps)
-        follower_state = LogState(term=1, index=1, last_term=1, commit_index=1, leader_id=node_1.uri)
-        for nid in self.nodes_by_id:
+        comms_op_4b = CommsOp(MessageCode.append_entries_response, CommsEdge.after_send)
+        action_4b = ActionOnMessage(comms_op=comms_op_2b, action_code=ActionCode.pause)
+        for nid in self.uris_by_id:
             if nid != 1:
-                node = self.nodes_by_id[nid]
-                phase_4_steps.append(PhaseStep(node.uri, validate_class=ValidateState(log_state=follower_state)))
+                node = self.node_by_id(nid)
+                phase_4_steps.append(PhaseStep(node.uri, runner_class=action_4b))
         phase_4 = Phase(phase_4_steps, description="Leader handles all heartbeat responses and followers see commit")
         self.add_phase(phase_4)
+
+        phase_5_steps = []
+        
+        phase_5_steps.append(PhaseStep(node_1.uri, do_now_class=NoOp()))
+        follower_state = LogState(term=1, index=1, last_term=1, commit_index=1, leader_id=node_1.uri)
+        for nid in self.uris_by_id:
+            if nid != 1:
+                node = self.node_by_id(nid)
+                phase_5_steps.append(PhaseStep(node.uri, validate_class=ValidateState(log_state=follower_state)))
+        phase_5 = Phase(phase_5_steps, description="Validating state at followers shows commit of term start log record")
+        self.add_phase(phase_5)
