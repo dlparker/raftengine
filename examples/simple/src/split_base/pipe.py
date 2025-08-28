2711a20d7ba3bf22b8a6333d1fbe21eea43e4a2d
Dennis Parker <dennis@windhavengroup.com>
2025-08-28 11:51:25 -0500

    removed examples/simple

--- examples/simple/src/split_base/pipe.py
+++
@@ -1,9 +0,0 @@
-
-
-class FakeRPCPipe:
-
-    def __init__(self, dispatcher):
-        self.dispatcher = dispatcher
-
-    async def issue_command(self, command, timeout=1.0):
-        return await self.dispatcher.route_command(command)
