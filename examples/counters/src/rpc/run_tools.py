bda17c5a5a0e2e464fb2b0370ec3b4e73e6bfcc4
Dennis Parker <dennis@windhavengroup.com>
2025-09-12 14:56:32 -0500

    examples counters tests working again

--- examples/counters/src/rpc/run_tools.py
+++ examples/counters/src/rpc/run_tools.py
@@ -5,8 +5,11 @@ from base.validator import Validator
 from split_base.collector import Collector
 from split_base.dispatcher import Dispatcher
 from rpc.raft_stub import RaftServerStub
-from rpc.rpc_client import RPCClient
-from rpc.rpc_server import RPCServer
+#from rpc.rpc_client import RPCClient
+#from rpc.rpc_server import RPCServer
+
+from raftengine.extras.astream_rpc import RPCClient
+from raftengine.extras.astream_rpc import RPCServer
 
 class RunTools:
 
