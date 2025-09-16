17ff0ecf0f8a50fc21f4193e3141a442187adde7
Dennis Parker <dennis@windhavengroup.com>
2025-09-16 08:13:01 -0500

    log and rpc implementation pull into raftengine done

---
+++ raftengine/extras/__init__.py
@@ -0,0 +1,14 @@
+import abc
+class RPCClientAPI(abc.ABC):
+
+    @abc.abstractmethod
+    def __init__(self, host, port, timeout=1.0):
+        raise NotImplementedError
+
+class RPCServer(abc.ABC):
+
+    @abc.abstractmethod
+    def __init__(self, raft_server, rm_wait_for_result=False):
+        raise NotImplementedError
+
+    
